4a16efc7beab50d3f5f74f2567b942bacc6d74a9
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2014-10-30 22:03:30 -0400

    XML to CSV for input compatible for david blie script

--- xmltocsv_title.py
+++ xmltocsv_title.py
@@ -1,25 +1,35 @@
 from lxml import etree
+from collections import Counter
 import csv
 import os
 import numpy
 
 def getTitle():
-    rows= []
 
-    for i in xrange(1,100):
-        file_path = os.getcwd() + '/womens-accessories-general/' + str(i)
-        
-        if os.path.exists(file_path):
-            xml_file = open(file_path)
-            xml_tree = etree.parse(xml_file)
-            item_ids = xml_tree.xpath('//ItemID/text()')
-            titles = xml_tree.xpath('//Title/text()')
-            for i in xrange(len(item_ids)):
-                rows.append([item_ids[i], titles[i]])
+    with open('./sample-data/toys-and-hobbies/titles.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter=',')
+        for root, dirs, files in os.walk('./sample-data/toys-and-hobbies/'):
+            rows= []
+            for i in xrange(1,100):
+                file_path = root + '/' + str(i)
+                print file_path
 
-    with open('titles.csv', 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerows([[column.encode("UTF-8") for column in row] for row in rows])
+                if os.path.exists(file_path):
+                    xml_file = open(file_path)
+                    try:
+                        xml_tree = etree.parse(xml_file)
+                        item_ids = xml_tree.xpath('//ItemID/text()')
+                        titles = xml_tree.xpath('//Title/text()')
+                    except etree.XMLSyntaxError:
+                        print "Error:" + file_path
+                    finally:
+                        xml_file.close()
+                    for i in xrange(len(item_ids)):
+                        titleFreq = Counter(titles[i].split()).items()
+                        titleFormat = str(len(titleFreq)) + ' '
+                        for term, freq in titleFreq:
+                            titleFormat += term.encode("UTF-8") + ':' + str(freq) + ' '
+                        f.write(titleFormat + '\n')
 
 if __name__ == "__main__":
     getTitle()
