99e128f05aa9c4a7ad76d1fea0ba4562043ba589
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2014-11-24 16:36:33 -0500

    TMVE compatible data generation

--- xmltocsv_title.py
+++ xmltocsv_title.py
@@ -6,30 +6,43 @@ import numpy
 
 def getTitle():
 
-    with open('./sample-data/toys-and-hobbies/titles.csv', 'wb') as f:
-        writer = csv.writer(f, delimiter=',')
-        for root, dirs, files in os.walk('./sample-data/toys-and-hobbies/'):
-            rows= []
-            for i in xrange(1,100):
-                file_path = root + '/' + str(i)
-                print file_path
+    with open('./sample-data/toys-and-hobbies/toys-and-hobbies.dat', 'wb') as f:
+        with open('./sample-data/toys-and-hobbies/toys-and-hobbies-vocab.dat', 'wb') as v:
+            with open('./sample-data/toys-and-hobbies/toys-and-hobbies-docs.dat', 'wb') as d:
+                # writer = csv.writer(f, delimiter=',')
+                vocab = []
+                for root, dirs, files in os.walk('./sample-data/toys-and-hobbies/'):
+                    rows= []
+                    for i in xrange(1,10):
+                        file_path = root + '/' + str(i)
+                        print file_path
 
-                if os.path.exists(file_path):
-                    xml_file = open(file_path)
-                    try:
-                        xml_tree = etree.parse(xml_file)
-                        item_ids = xml_tree.xpath('//ItemID/text()')
-                        titles = xml_tree.xpath('//Title/text()')
-                    except etree.XMLSyntaxError:
-                        print "Error:" + file_path
-                    finally:
-                        xml_file.close()
-                    for i in xrange(len(item_ids)):
-                        titleFreq = Counter(titles[i].split()).items()
-                        titleFormat = str(len(titleFreq)) + ' '
-                        for term, freq in titleFreq:
-                            titleFormat += term.encode("UTF-8") + ':' + str(freq) + ' '
-                        f.write(titleFormat + '\n')
+                        if os.path.exists(file_path):
+                            xml_file = open(file_path)
+                            try:
+                                xml_tree = etree.parse(xml_file)
+                                item_ids = xml_tree.xpath('//ItemID/text()')
+                                titles = xml_tree.xpath('//Title/text()')
+                            except etree.XMLSyntaxError:
+                                print "Error:" + file_path
+                            finally:
+                                xml_file.close()
+                            for i in xrange(len(item_ids)):
+                                # titleFreq = Counter(titles[i].split()).items()
+
+                                # titleFormat = str(len(titleFreq)) + ' '
+
+                                # for term, freq in titleFreq:
+                                #     if term not in vocab:
+                                #         index = len(vocab)
+                                #         vocab.append(term.encode("UTF-8"))
+                                #     else:
+                                #         index  = vocab.index(term)
+                                #     titleFormat += str(index) + ':' + str(freq) + ' '
+                                # f.write(titleFormat + '\n')
+                                d.write(titles[i].encode("UTF-8") + '\n')
+            # for term in vocab:
+            #     v.write("%s\n" % term)
 
 if __name__ == "__main__":
     getTitle()
